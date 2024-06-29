161d0379dbfca5b79053b3464078ea54b9e4ee9e
CakmLexi <calmlexi@hotmail.com>
2024-06-29 14:29:00 +0800

    feat: 新增类型文件

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { fileURLToPath } from 'url'
 import protobufjs from 'protobufjs'
 
 const filePath = fileURLToPath(import.meta.url)
+/**
+ * - proto 文件路径
+ */
 const proto = path.dirname(filePath) + '/kritor/protos'
 
 export { protobufjs, proto }
